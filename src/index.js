e3796d685f3f53a6f00bc27b472e67f42e05e841
Franco Ruggeri <francorugga12@gmail.com>
2022-11-02 12:32:49 -0300

    agregue bootstrap js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
